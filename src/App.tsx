c7a879de249e9b3137b8579904e07926e837f809
ashmadev <ashmadev@protonmail.com>
2022-10-12 21:25:12 +0200

    Remove unnecessary provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
 import Header from "./containers/header";
 import Footer from "./containers/footer";
 import MainPage from "./pages/main-page";
 import "./styles/App.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Header />
       <MainPage />
       <Footer />
-    </QueryClientProvider>
+    </>
   );
 }
 
